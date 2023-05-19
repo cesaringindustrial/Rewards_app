ee8eab032cad61e5579017661f6ec8cf00c9cc7c
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-05-19 07:27:27 -0500

    add ORM, DB Modules Question and Choice

--- premios_app/polls/models.py
+++ premios_app/polls/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+## Model Question DB no include id because per default include PK
+class Question(models.Model):
+    question_text= models.CharField(max_length=200)
+    pub_date= models.DateTimeField("date published")
+
+## Model Choices DB no include id because per default include PK
+## on_delete=models.CASCADE >>> When pass to next question this delete all answers in cascade.
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes= models.IntegerField(default=0)
+
+

3ee8f2f764d0cb237b343974085c702b382d1327
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-05-24 09:53:22 -0500

    add admin and models

--- premios_app/polls/models.py
+++ premios_app/polls/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+from django.utils import timezone
+import datetime
 ## Model Question DB no include id because per default include PK
 class Question(models.Model):
     question_text= models.CharField(max_length=200)
     pub_date= models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+##esta def es para definir cual fue el ultimo publicado    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
 
 ## Model Choices DB no include id because per default include PK
 ## on_delete=models.CASCADE >>> When pass to next question this delete all answers in cascade.
@@ -11,5 +20,7 @@ class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes= models.IntegerField(default=0)
-
+    
+    def __str__(self):
+        return self.choice_text
 

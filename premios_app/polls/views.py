01b8e379b6bb075e54a852940fcddc16e0ed50aa
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-05-24 12:16:24 -0500

    add views and urls

--- premios_app/polls/views.py
+++ premios_app/polls/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hola mundo")
+    latest_question_list= Question.objects.all()
+    return render(request, "polls/index.html",{"latest_question_list":latest_question_list})
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+def result(request, question_id):
+    return HttpResponse(f"Estás viendo las respuestas número {question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando la pregunta número {question_id}")
 

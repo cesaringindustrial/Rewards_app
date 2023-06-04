db48cd006c715239aa80dd23f4f36449e89f3777
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-06-04 04:53:18 -0500

    add urls and views with funcionality

--- premios_app/polls/urls.py
+++ premios_app/polls/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     #ex:/polls/5/
     path("<int:question_id>/detail/platzi/cuestionario",views.detail, name="detail"),
      #ex:/polls/5/results/
-    path("<int:question_id>/results/",views.result, name="result"),
+    path("<int:question_id>/results/",views.result, name="results"),
      #ex:/polls/5/vote/
     path("<int:question_id>/vote/",views.vote, name="vote")
 ]
--- premios_app/polls/views.py
+++ premios_app/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse,HttpResponseRedirect
+from .models import Question, Choice
+from django.urls import reverse 
 
 def index(request):
     latest_question_list= Question.objects.all()
@@ -15,5 +16,18 @@ def result(request, question_id):
     return HttpResponse(f"Estás viendo las respuestas número {question_id}")
 
 def vote(request, question_id):
-    return HttpResponse(f"Estás votando la pregunta número {question_id}")
+    question= get_object_or_404(Question,pk= question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError,Choice.DoesNotExist):
+        return render(request,"polls/detail.html",{
+            'question':question,
+            'error_mesagge':"no elegiste una respuesta"
+        })
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
 
+
+       

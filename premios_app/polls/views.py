ea1376f9c63561eb7fe5d893d4f949e8d92999a4
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-06-04 06:41:13 -0500

    add urls and views with generic funtions

--- premios_app/polls/views.py
+++ premios_app/polls/views.py
@@ -1,19 +1,42 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse 
+from django.views import generic
 
-def index(request):
-    latest_question_list= Question.objects.all()
-    return render(request, "polls/index.html",{"latest_question_list":latest_question_list})
+class IndexView(generic.ListView):
+    template_name= "polls/index.html"
+    context_object_name= "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model= Question
+    template_name= "polls/detail.html"
+
+class ResultView(generic.DetailView):
+    model= Question
+    template_name= "polls/results.html"
+
+#def index(request):
+#    latest_question_list= Question.objects.all()
+ #   return render(request, "polls/index.html",{"latest_question_list":latest_question_list})
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk= question_id )
-    return render (request,"polls/detail.html",{"question": question})
+#def detail(request, question_id):
+ #   question = get_object_or_404(Question, pk= question_id )
+  #  return render (request,"polls/detail.html",{"question": question})
     
-def result(request, question_id):
-    return HttpResponse(f"Estás viendo las respuestas número {question_id}")
+#def result(request, question_id):
+ #   question= get_object_or_404(Question,pk= question_id)
+  #  return render(request,"polls/results.html",{
+   #         'question':question
+    #})
+ 
 
 def vote(request, question_id):
     question= get_object_or_404(Question,pk= question_id)

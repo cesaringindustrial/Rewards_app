55021655fc13e67abc17aeda507d20aeed24bb94
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-06-20 06:04:02 -0500

    add test 1 and 2

--- premios_app/polls/tests.py
+++ premios_app/polls/tests.py
@@ -1,3 +1,20 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse 
 
-# Create your tests here.
+from .models import Question
+
+class QuestionModelTests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        """Was_publised_recently returns False for questions whose pub_date is the future"""
+        time= timezone.now() + datetime.timedelta(days=30)
+        future_question= Question(question_text="Quien es el mejor CD de platzi",pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
+
+class QuestionIndexViewTests(TestCase):
+    def test_no_questions(self):
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])

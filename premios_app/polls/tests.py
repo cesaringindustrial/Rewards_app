71c0b96a6563ac36030642961148948f68193a4b
César Augusto Montañez E <cesaringindustrial@gmail.com>
2023-06-20 06:43:19 -0500

    add test 3 and 4 future and past Question

--- premios_app/polls/tests.py
+++ premios_app/polls/tests.py
@@ -12,9 +12,25 @@ class QuestionModelTests(TestCase):
         future_question= Question(question_text="Quien es el mejor CD de platzi",pub_date=time)
         self.assertIs(future_question.was_published_recently(),False)
 
+def create_question(question_text,days):
+    time= timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text= question_text,pub_date=time)
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         response = self.client.get(reverse("polls:index"))
         self.assertEqual(response.status_code,200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])
+
+    def test_future_question(self):
+        create_question("Future question", days= 30)
+        response= self.client.get(reverse("polls:index"))
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerysetEqual(response.context["latest_question_list"],[])
+
+    def test_past_question(self): 
+        create_question("Past question", days= 30)
+        response= self.client.get(reverse("polls:index"))
         self.assertContains(response, "No polls are available.")
         self.assertQuerysetEqual(response.context["latest_question_list"],[])
